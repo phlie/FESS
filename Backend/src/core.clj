2f272cebeee53d06ba78327dc72e5c03e56858cf
phlie <matt.m.stein@gmail.com>
2019-04-12 06:09:30 -0700

    Fixed the bugs and reduced the lag
    There were a couple of errors in the simulation including a very dumb one with setting the acceleration and position on the backend. Also, made it only make 3 http requests a second since too many requests means the data is not transmitted. Now it runs smoothly at 30 FPS but has a lag time between the backend and frontend of a third of a second.

--- Backend/src/core.clj
+++ Backend/src/core.clj
@@ -1,33 +1,89 @@
-(ns core
-  (:use [ring.adapter.jetty]
+(ns core                                ; The main namespace
+  (:use [ring.adapter.jetty]            ; All of the libraries used
         [ring.middleware.params]
         [ring.middleware.json :refer [wrap-json-response]]
         [ring.util.response])
-  (:gen-class))
+  (:gen-class))                         ; Generates compiled bytecode for the 'core' namespace
 
-(def t 0)
-(def a -9.81)
-(def v 500)
+(def frames-sent 10)                    ; The current amount of data to send between exchange
+(def frames-per-second 30)              ; The current number of frames of the frontend per second
 
-(defn velocity
+;; This function is used at the start of the frontend
+;; simulator running to reset all the variables
+(defn init-vars
+  "The variables used in the simulation"
   []
-  (def v (+ v (* a (/ t 15))))
-  (def t (inc t))
-  v)
+  (def t 0)                             ; 1 Frame, currently set to 15 frames a second
+  (def a -9.81)                         ; Meters per second per second
+  (def vx 20.0)                         ; The various velocities
+  (def vy 10.0)                         ; Meters per second
+  (def vz -5.0)
+  (def px [])                            ; The various positions
+  (def py [])                      ; X Y Z, with Y being straight up
+  (def pz [])
+  (def x 0)                             ; The current position in all the axeses
+  (def y 0)
+  (def z 0)
+  (def rx 0)                            ; The current rotation in the 3 dimensions
+  (def ry 0)
+  (def rz 0))
 
-(defn handler [{{data "data"} :params}]
-  (-> (response {:velocity (velocity)})
-      (content-type "text/html")
+;; Computes the velocity at every frame of the rocket
+;; then in the front end times by a frame delta
+(defn velocity
+  "Used to determine the velocity of the rocket"
+  []
+  (def v (+ v (* a (/ t frames-per-second)))) ; The equation of motion for a projectile launched with a starting velocity
+  (def t (inc t))                             ; Increments the current time with every frontend frame
+  {:velocity v})                                          ; Return the current velocity
+
+(defn position-and-rotation
+  "Used to get the final position vector and rotation vector"
+  []
+  (def px [])                           ; Initialize all the arrays to empty
+  (def py [])
+  (def pz [])
+  (dotimes [n frames-sent]              ; Loop through the amount of data to send
+    (def vy (+ vy (* a (/ 1 frames-per-second)))) ; Calculate the velocity
+    (def x (+ x (* vx (/ 1 frames-per-second))))  ; Calculate the new position
+    (def y (+ y (* vy (/ 1 frames-per-second))))
+    (def z (+ z (* vz (/ 1 frames-per-second))))
+    ;; (println "Position: " py "Velocity: " v)
+    (def px (conj px x))                ; Append the array with the newest value
+    (def py (conj py y))
+    (def pz (conj pz z))
+    ;; (println y)
+    ;; (println py)     ; Used for debugging
+    (def t (inc t)))                    ; Increment the time by 1
+  {:px px :py py :pz pz                 ; Send all the information the Frontend expects
+   :rx rx :ry ry :rz rz})
+
+
+
+;; Used as to receive and send data between the frontend and the backend
+(defn handler [{{reset "reset"} :params}]
+  "The main server handler"
+  ;; (println reset)
+  (when (= reset "y") (init-vars))
+                                        ; If the frontend sents a reset signal, reset the variables
+  (-> (response (position-and-rotation)) ; Send, for now, the velocity back to the frontend
+      ;; (content-type "text/html")     ; Before it was sending text
+      (content-type "application/json") ; It is currently sending JSON
+      ;; Needs to work on the same local machine, port 3000
       (header "Access-Control-Allow-Origin" "http://localhost:3000")))
 
+;; Wraps the handlers key and values as JSON data, and then wraps the parameters such as content-type
 (def app
   (-> (wrap-json-response handler) wrap-params))
 
 
+;; The main function for the project, called with 'boot run'
 (defn -main
   "This is the entry point"
   []
   (println "Hello Earth")
+  (init-vars)                     ; Just in case the frontend doesn't
   ;; Can be stopped by running (.stop my-server)
+  ;; This function starts the server and makes it keep running without shutting down
   (def my-server (run-jetty app {:port 3000 :join? true})))
 
--- Frontend/LaunchSite.gd
+++ Frontend/LaunchSite.gd
@@ -1,28 +1,54 @@
 extends Spatial
 
 # Declare member variables here. Examples:
-var velocity = 1
+var position = Vector3()
+
+var pxArray = []
+var px = []
+var pyArray = []	# Used for temporary storage until the next array replace
+var py = []			# The array used by the simulation
+var pzArray = []
+var pz = []
+
 var currentCam = 0
+var currentFrame = 0
+var setupAndReady = false	# Used to wait 10 frames for the simulation to get going
 # var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$HTTPRequest.request("http://0.0.0.0:3000")
+	var query = "reset=y"	# The required post data to be transmitted when the frontend starts
+	# Sets up an HTTP request with the address, headers, SSL or not, Method, and the Post parameters
+	$HTTPRequest.request("http://0.0.0.0:3000", ["Content-Type: application/x-www-form-urlencoded"], false, HTTPClient.METHOD_POST, query)
 	$EarthAlpha/FollowCam.make_current()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$HTTPRequest.request("http://0.0.0.0:3000")
-	var position = Vector3()
-	position.y += delta*velocity
-	$EarthAlpha.global_translate(position)
+	if setupAndReady:						# When setup, begin the simulation
+		position.x = px[currentFrame]		# Loops through the used array
+		position.y = py[currentFrame]		# Loops through the used array
+		position.z = pz[currentFrame]		# Loops through the used array
+		print(position.y)
+		$EarthAlpha.global_translate(position) # Move the rocket according to its location
+	currentFrame += 1									# Increment the current frames
+	if currentFrame	== 10:
+		$HTTPRequest.request("http://0.0.0.0:3000")		# Starts a network communication once every n frames, too many and it lags
+		setupAndReady = true							# A flag for the simulation to start
+		px = pxArray									# Every 10 frames, use a new array of positions
+		py = pyArray
+		pz = pzArray
+		currentFrame = 0								# Set the currentFrame back to 0 for array indexing
+#		print(py)
 
-
-
+# The function that deals with the returns from the backend in an async sort of way
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
-    var json = JSON.parse(body.get_string_from_utf8())
-    velocity = json.result["velocity"]
+	var json = JSON.parse(body.get_string_from_utf8())
+	pxArray = json.result["px"]
+	pyArray = json.result["py"]
+	pzArray = json.result["pz"]
+	# position = Vector3(json.result["px"], json.result["py"], json.result["pz"])
 
+# Depending on which one of the arrow keys is pressed, switch to that camera
 func _input(ev):
 	if Input.is_key_pressed(KEY_LEFT):
 		$Ground/GroundCam.make_current()
